dccf2d4bc2854d4b2cb470834bc659ecb281f35f
ayoubessafi <57940108+ayoubessafi@users.noreply.github.com>
2021-02-15 00:40:00 +0100

    Ajoutez des fichiers projet.

---
+++ Gestion Etudiant/Data/Gestion_EtudiantContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Gestion_Etudiant.Models;
+
+namespace Gestion_Etudiant.Data
+{
+    public class Gestion_EtudiantContext : DbContext
+    {
+        public Gestion_EtudiantContext (DbContextOptions<Gestion_EtudiantContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Gestion_Etudiant.Models.StudentModel> StudentModel { get; set; }
+    }
+}
---
+++ Gestion Etudiant/Migrations/20210214212649_FirstSetUp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Gestion_Etudiant.Migrations
+{
+    public partial class FirstSetUp : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "StudentModel",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    CIN = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudentModel", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "StudentModel");
+        }
+    }
+}
